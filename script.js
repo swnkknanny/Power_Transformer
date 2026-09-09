061ea115ca369ea08c407975353affda4509bf29
Suwanan <swnkk13@gmail.com>
2026-09-09 10:29:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentChart = null;
 let currentActiveFilter = 'all';
 
 // Security Protocol Credentials
-const ADMIN_PASSWORD = '1234'; 
+const ADMIN_PASSWORD = '13102547'; 
 let isAdmin = false;
 
 const DEFAULT_EXCEL_FILE = 'ALL_RAM.xlsx';
